6e8ee23b083e6ac05783f2efc9efbb849646f275
GentilSilva <silvagentil.sg@gmail.com>
2022-11-06 09:49:23 -0300

    LTP1 lista8

---
+++ 1_semestre/LTP1/exercicios/src/lista8/Atividade9.java
@@ -0,0 +1,71 @@
+package lista8;
+
+import java.util.Scanner;
+
+public class Atividade9 {
+
+	public static void main(String[] args) {
+		
+		double total = 0;
+		double faixa1 = 0;
+		double faixa2 = 0;
+		double faixa3 = 0;
+		double faixa4 = 0;
+		double faixa5 = 0;
+		int opcao, idade;
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		do {
+			System.out.print("Menu: \n"
+			+ "1 - Informar idade \n"
+			+ "2 - Para encerrar o programa \n"
+			+ "Digite: ");
+			opcao = scan.nextInt();
+			
+			if(opcao == 1) {
+				System.out.print("Digite a idade: ");
+				idade = scan.nextInt();
+				if(idade <= 17) {
+					total += idade;
+					faixa1 += idade;
+				} else if(idade <= 35) {
+					total += idade;
+					faixa2 += idade;
+				} else if(idade <= 50) {
+					total += idade;
+					faixa3 += idade;
+				} else if(idade <= 65) {
+					total += idade;
+					faixa4 += idade;
+				} else {
+					total += idade;
+					faixa5 += idade;
+				}
+			} else if(opcao > 2){
+				System.out.println("Opcao invalida");
+			}
+			
+		} while(opcao != 2);
+		
+		faixa1 = faixa1 * 100 / total;
+		faixa2 = faixa2 * 100 / total;
+		faixa3 = faixa3 * 100 / total;
+		faixa4 = faixa4 * 100 / total;
+		faixa5 = faixa5 * 100 / total;
+		
+		String mensagem = String.format("A porcentagem de pessoas com idade ate 17 anos e: %.2f \n"
+				+ "A porcentagem de pessoas entre 18 e 35 anos e: %.2f \n"
+				+ "A porcentagem de pessoas entre 36 e 50 anos e: %.2f \n"
+				+ "A porcentagem de pessoas entre 51 e 65 anos e: %.2f \n"
+				+ "A porcentagem de pessoas com mais de 65 anos e: %.2f", faixa1, faixa2, faixa3, faixa4, faixa5);
+		
+		System.out.println(mensagem);
+		System.out.println("Programa encerrado!");
+		
+		scan.close();
+		
+	}
+	
+}
