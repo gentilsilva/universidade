fdd2b3d02368326230eb467c54fb52d7c84e39ab
GentilSilva <silvagentil.sg@gmail.com>
2022-11-22 17:48:06 -0300

    atividade4 ipw

--- 1_semestre/IPW/atividade4/javascript/script.js
+++ 1_semestre/IPW/atividade4/javascript/script.js
@@ -1,110 +1,58 @@
-let submitButton = document.getElementById("btn-submit")
-let verificou = false;
+let cpfValue = document.querySelector('.cpf');
+let verificouPrimeiroDigito = 0;
+let verificouSegundoDigito = 0;
 
 function verificaPrimeiroDigito(valor) {
-    let primeiroDigito = 0;
+    let primeiroDigito = valor[valor.length - 2];
     let verificaPrimeiroDigito = 0;
     let multiplicador = 10;
-    let valorString = valor.toString();
-    let tamanhoValor = valorString.length;
-    let valorArr = [];
 
-    for(let i = 0; i < valorString.length; i++) {
-        valorArr.push(valorString[i])
-        if(i == valorString.length - 2) {
-            primeiroDigito = valorString[i];
-        }
-    }
-    for(let i = 0; i < tamanhoValor - 2; i++) {
-        verificaPrimeiroDigito += valorArr[i] * multiplicador;
+    for(let i = 0; i < valor.length - 2; i++) {
+        verificaPrimeiroDigito += valor[i] * multiplicador;
         multiplicador--;
     }
 
     verificaPrimeiroDigito = verificaPrimeiroDigito % 11;
 
-    if(verificaPrimeiroDigito != 0 || verificaPrimeiroDigito != 1) {
-        verificaPrimeiroDigito = 11 - verificaPrimeiroDigito;
-    }
+    let valorFinal = (verificaPrimeiroDigito != 0 || verificaPrimeiroDigito != 1) ? 
+    11 - verificaPrimeiroDigito : verificaPrimeiroDigito;
 
-    if(verificaPrimeiroDigito == primeiroDigito) {
-        verificou = true;
-    } else {
-        verificou = false
-    }
+    return valorFinal == primeiroDigito ? verificouPrimeiroDigito = 1 
+        : verificouPrimeiroDigito = 0;
+
 }
 
 function verificaSegundoDigito(valor) {
-    let segundoDigito = 0;
+    let segundoDigito = valor[valor.length - 1];
     let verificaSegundoDigito = 0;
     let multiplicador = 10;
-    let valorString = valor.toString();
-    let tamanhoValor = valorString.length;
-    let valorArr = [];
 
-    for(let i = 0; i < valorString.length; i++) {
-        valorArr.push(valorString[i])
-        if(i == valorString.length - 1) {
-            segundoDigito = valorString[i];
-        }
-    }
-    for(let i = 1; i < tamanhoValor - 1; i++) {
-        verificaSegundoDigito += valorArr[i] * multiplicador;
+    for(let i = 1; i < valor.length - 1; i++) {
+        verificaSegundoDigito += valor[i] * multiplicador;
         multiplicador--;
     }
 
     verificaSegundoDigito = verificaSegundoDigito % 11;
 
-    if(verificaSegundoDigito != 0 || segundoDigito != 1) {
-        verificaSegundoDigito = 11 - verificaSegundoDigito;
-    }
+    let valorFinal = (verificaSegundoDigito != 0 || verificaSegundoDigito != 1) ? 
+        11 - verificaSegundoDigito : verificaSegundoDigito;
 
-    if(verificaSegundoDigito == segundoDigito) {
-        verificou = true;
-    } else {
-        verificou = false
-    }
-
-}
-
-function verificaCpf() {
-    if(verificou == true) {
+    return valorFinal == segundoDigito ? verificouSegundoDigito = 1 
+        : verificouSegundoDigito = 0;
         
-    }
 }
 
-
-// function verificaCpf(valor) {
-//     let primeiroDigito = 0;
-//     let segundoDigito = 0;
-//     let multiplicador = 10;
-//     let valorString = valor.toString();
-//     let tamanhoValor = valorString.length;
-//     let valorArr = [];
-
-//     for(let i = 0; i < valorString.length; i++) {
-//         valorArr.push(valorString[i])
-//     }
-//     for(let i = 0; i < tamanhoValor - 2; i++) {
-//         primeiroDigito += valorArr[i] * multiplicador;
-//         multiplicador--;
-//     }
-
-//     primeiroDigito = primeiroDigito % 11;
-//     multiplicador = 10;
-
-//     for(let i = 1; i < tamanhoValor - 1; i++) {
-//         segundoDigito += valorArr[i] * multiplicador;
-//         multiplicador--;
-//     }
-
-//     segundoDigito = segundoDigito % 11;
-
-//     if(primeiroDigito != 0 || primeiroDigito != 1) {
-//         primeiroDigito = 11 - primeiroDigito;
-//     }
-//     if(segundoDigito != 0 || segundoDigito != 1) {
-//         segundoDigito = 11 - segundoDigito;
-//     }
-
-//     console.log(primeiroDigito);
-// }
+function validaCpf() {
+    verificaPrimeiroDigito(cpfValue.value)
+    verificaSegundoDigito(cpfValue.value)
+    if(verificouPrimeiroDigito == 1 && verificouSegundoDigito == 1) {
+        document.getElementById('cpfValidado').style.display = "block";
+        document.getElementById('cpfInvalidado').style.display = "none";
+        document.getElementById('btn-submit').disabled = false;
+        return;
+    } else {
+        document.getElementById('cpfInvalidado').style.display = "block";
+        document.getElementById('cpfValidado').style.display = "none";
+        document.getElementById('btn-submit').disabled = true;
+    }
+}
